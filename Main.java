75bfb4aff3f95f052ab22fe1666985f3dd4a5671
ikeda0927 <ikeda.kohei4477@gmail.com>
2019-06-19 13:16:13 +0900

    add counter2

--- Main.java
+++ Main.java
@@ -5,15 +5,24 @@ public class Main {
     private static int counter1 =0;
     private static int COUNTER1 =10;
     private static boolean counter1Judge=true;
+    private static int counter2 =0;
+    private static int COUNTER2 =20;
+    private static boolean counter2Judge=true;
 
     public static void main(String[] args) {
 	// write your code here
-        while(counter1Judge){
+        while(counter1Judge || counter2Judge){
             if(counter1<COUNTER1){
                 counter1++;
             }else{
                 System.out.println("counter1 Max");
                 counter1Judge = false;
+            }
+            if(counter2<COUNTER1){
+                counter2++;
+            }else{
+                System.out.println("counter2 Max");
+                counter2Judge = false;
             }
         }
     }
