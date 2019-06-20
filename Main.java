5d19479a91bab06a35b24f96f8e772b42bf25889
池田 康平 <ikeda.kohei4477@gmail.com>
2019-06-20 21:38:51 +0900

    add counter4

--- Main.java
+++ Main.java
@@ -8,6 +8,9 @@ public class Main {
     private static int counter2 =0;
     private static int COUNTER2 =20;
     private static boolean counter2Judge=true;
+    private static int counter4 =0;
+    private static int COUNTER4 =40;
+    private static boolean counter4Judge=true;
 
     public static void main(String[] args) {
 	// write your code here
